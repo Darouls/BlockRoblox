d9466c2c217b7c3486fef3c95a7dea4017310af3
darouls <darouls@gmail.com>
2026-01-20 00:03:03 +0100

    modifying logdir of proxyuser script

--- ProxyUser.ps1
+++ ProxyUser.ps1
@@ -1,18 +1,24 @@
 $HomeProxyIP = "192.168.1.11"
 $ProxyPort = "3128"
 
+$LogDir = "$env:LOCALAPPDATA\NetworkProxy"
+$LogFile = "$LogDir\proxy-user.log"
+
+if (!(Test-Path $LogDir)) {
+    New-Item -ItemType Directory -Path $LogDir -Force | Out-Null
+}
+
 $IsHome = Test-NetConnection -ComputerName $HomeProxyIP -Port $ProxyPort -InformationLevel Quiet
 
-$log = "C:\ProgramData\proxy-user.log"
-Add-Content $log "$(Get-Date) - Test maison = $IsHome"
+Add-Content $LogFile "$(Get-Date) - Test maison = $IsHome"
 
 if ($IsHome) {
     Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' ProxyEnable 1
     Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' ProxyServer "$HomeProxyIP:$ProxyPort"
-    Add-Content $log "Proxy ACTIVÉ"
+    Add-Content $LogFile "Proxy ACTIVÉ"
 }
 else {
     Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' ProxyEnable 0
     Remove-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' ProxyServer -ErrorAction SilentlyContinue
-    Add-Content $log "Proxy DÉSACTIVÉ"
+    Add-Content $LogFile "Proxy DÉSACTIVÉ"
 }
